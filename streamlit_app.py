e3fbeadc1ef81023692b211efce9d6084b1e0ea8
Roberts-Sion <146732670+Roberts-Sion@users.noreply.github.com>
2026-09-24 16:14:46 +0000

    Intro paragraph

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 st.title("Analysis of ENA Innovation Projects")
-st.write("This analysis...")
+st.write("This analysis provides insight into previous and current Innovation Projects funded by Ofgem.\
+         Data regarding these projects, accessed using Energy Networks Association's Smarter Networks Portal,\
+         is accessible through a series of different visual and graphical means, and adjustable to the users demand.\
+         Information regarding the analyses conducted and methods used can be found with each plot.")
+
 
 st.write("Last updated 24/09/2026")
