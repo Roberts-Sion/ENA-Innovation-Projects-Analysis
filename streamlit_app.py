e636f7ac5f1a32abd947f0c085fae288472fb621
Roberts-Sion <146732670+Roberts-Sion@users.noreply.github.com>
2026-09-25 10:27:42 +0000

    Removing Module imports/Operational Check

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ st.write("This analysis provides insight into previous and current Innovation Pr
          Data regarding these projects, accessed using Energy Networks Association's Smarter Networks Portal,\
          is accessible through a series of different visual and graphical means, and adjustable to the users demand.\
          Information regarding the analyses conducted and methods used can be found with each plot.")
-
+st.write("This is to check the website is operating correctly.")
 
 st.write("Last updated 24/09/2026")
